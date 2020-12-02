607668faa2c6a58da8306d40274f9b03480adea8
adot08 <im.lzz@sjtu.edu.cn>
2020-12-02 09:42:50 +0800

    input resize 256

--- src/dataset.py
+++ src/dataset.py
@@ -4,6 +4,7 @@ from PIL import Image
 from torchvision.transforms import Compose, Resize, RandomCrop, CenterCrop, RandomHorizontalFlip, ToTensor, Normalize, transforms
 import random
 import ipdb
+from torchvision.transforms.transforms import RandomVerticalFlip
 
 from options import TestOptions
 
@@ -49,7 +50,6 @@ class dataset_unpair(data.Dataset):
     images_A = os.listdir(os.path.join(self.dataroot, opts.phase + 'A'))
     self.A = [os.path.join(self.dataroot, opts.phase + 'A', x) for x in images_A]
     self.A_dict = self.slice_dict(images_A)
-
     # B
     images_B = os.listdir(os.path.join(self.dataroot, opts.phase + 'B'))
     self.B = [os.path.join(self.dataroot, opts.phase + 'B', x) for x in images_B]
@@ -60,14 +60,8 @@ class dataset_unpair(data.Dataset):
     self.dataset_size = max(self.A_size, self.B_size)
     self.input_dim_A = opts.input_dim_a
     self.input_dim_B = opts.input_dim_b
-
     # setup image transformation
-    # transforms = [Resize((opts.resize_size, opts.resize_size), Image.BICUBIC)]
-    transforms = []
-    if opts.phase == 'train':
-      transforms.append(RandomCrop(opts.crop_size))
-    # else:
-    #   transforms.append(RandomCrop(opts.crop_size))
+    transforms = [Resize((opts.resize_size, opts.resize_size), Image.BICUBIC)]
     if not opts.no_flip:
       transforms.append(RandomHorizontalFlip())
     transforms.append(ToTensor())
@@ -87,13 +81,9 @@ class dataset_unpair(data.Dataset):
     return data_A, data_B
   
   def slice_select(self, A_case_index, B_case_index, slice_index):
-    k_mri, k_ct = len(self.A_dict[A_case_index])-1, len(self.B_dict[B_case_index])-1
-    m = random.randint(-5, 5)
+    k_mri, k_ct = len(self.A_dict[A_case_index]), len(self.B_dict[B_case_index])
     out_slice_index = int(int(slice_index) * (k_ct/k_mri))
-    if out_slice_index >= 5 and out_slice_index < (k_ct-5):
-      out_slice_index = out_slice_index + m
-    else:
-      out_slice_index = out_slice_index
+    out_slice_index = out_slice_index 
     return out_slice_index
 
   def slice_dict(self, MRI_list):
@@ -113,34 +103,32 @@ class dataset_unpair(data.Dataset):
   def load_img(self, img_name, input_dim):
     img = Image.open(img_name).convert('RGB')
     img = self.transforms(img)
-    if input_dim == 1:
-      img = img[0, ...] * 0.299 + img[1, ...] * 0.587 + img[2, ...] * 0.114
-      img = img.unsqueeze(0)
     return img
 
   def __len__(self):
     return self.dataset_size
 
 class dataset_pair(data.Dataset):
-  def __init__(self, opts):
+  def __init__(self, opts, phase_name):
     self.opts = opts
-    self.MR_files = os.listdir(os.path.join(self.opts.dataroot, self.opts.phase + 'A'))
-    self.CT_files = os.listdir(os.path.join(self.opts.dataroot, self.opts.phase + 'B'))
+    self.phase_name = phase_name
+    self.MR_files = os.listdir(os.path.join(self.opts.dataroot, self.phase_name + 'A'))
+    self.CT_files = os.listdir(os.path.join(self.opts.dataroot, self.phase_name + 'B'))
     self.CT_size = len(self.CT_files)
     self.MR_size = len(self.MR_files)
     assert self.CT_size == self.MR_size
 
     # setup image transformation
-    transforms = []
+    transforms = [Resize((opts.resize_size, opts.resize_size), Image.BICUBIC)]
     transforms.append(ToTensor())
     transforms.append(Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]))
     self.transforms = Compose(transforms)
     print('CT: %d, MR: %d images'%(self.CT_size, self.MR_size))
 
   def __getitem__(self, index):
-    MR = self.load_img(os.path.join(self.opts.dataroot, self.opts.phase+'A', self.MR_files[index]))
+    MR = self.load_img(os.path.join(self.opts.dataroot, self.phase_name+'A', self.MR_files[index]))
     CT_corresbonding_MR = 'CT'+self.MR_files[index].split('T1')[-1]
-    CT = self.load_img(os.path.join(self.opts.dataroot, self.opts.phase+'B', CT_corresbonding_MR))
+    CT = self.load_img(os.path.join(self.opts.dataroot, self.phase_name+'B', CT_corresbonding_MR))
     return MR, CT
 
   def load_img(self, img_name):
@@ -156,9 +144,9 @@ if __name__ == "__main__":
 
     from options import TrainOptions
     # parse options
-    parser = TestOptions()
+    parser = TrainOptions()
     opts = parser.parse()
 
-    dataset = dataset_pair(opts)
+    dataset = dataset_unpair(opts)
 
     print('done')
--- src/options.py
+++ src/options.py
@@ -8,8 +8,8 @@ class TrainOptions():
     self.parser.add_argument('--dataroot', type=str, default='/media/gdp/date/runze/codes/DRIT/datasets/slice', help='path of data')
     self.parser.add_argument('--phase', type=str, default='train', help='phase for dataloading')
     self.parser.add_argument('--batch_size', type=int, default=2, help='batch size')
-    self.parser.add_argument('--resize_size', type=int, default=0, help='resized image size for training')
-    self.parser.add_argument('--crop_size', type=int, default=(140,180), help='cropped image size for training')
+    self.parser.add_argument('--resize_size', type=int, default=256, help='resized image size for training')
+    self.parser.add_argument('--crop_size', type=int, help='cropped image size for training')
     self.parser.add_argument('--input_dim_a', type=int, default=3, help='# of input channels for domain A')
     self.parser.add_argument('--input_dim_b', type=int, default=3, help='# of input channels for domain B')
     self.parser.add_argument('--nThreads', type=int, default=8, help='# of threads for data loader')
@@ -52,7 +52,7 @@ class TestOptions():
     # data loader related
     self.parser.add_argument('--dataroot', type=str, required=True, help='path of data')
     self.parser.add_argument('--phase', type=str, default='test', help='phase for dataloading')
-    self.parser.add_argument('--resize_size', type=int, help='resized image size for training')
+    self.parser.add_argument('--resize_size', type=int, default=256, help='resized image size for training')
     self.parser.add_argument('--crop_size', type=int, help='cropped image size for training')
     self.parser.add_argument('--nThreads', type=int, default=4, help='for data loader')
     self.parser.add_argument('--input_dim_a', type=int, default=3, help='# of input channels for domain A')
@@ -68,7 +68,7 @@ class TestOptions():
     self.parser.add_argument('--concat', type=int, default=1, help='concatenate attribute features for translation, set 0 for using feature-wise transform')
     self.parser.add_argument('--no_ms', action='store_true', help='disable mode seeking regularization')
     self.parser.add_argument('--resume', type=str, help='specified the dir of saved models for resume the training')
-    self.parser.add_argument('--resume_path', type=str, required=True, help='specified the dir of saved models for resume the training')
+    self.parser.add_argument('--resume_path', type=str, help='specified the dir of saved models for resume the training')
     self.parser.add_argument('--gpu', type=int, default=0, help='gpu')
 
   def parse(self):
